992ebed756dce7989b86a408929d6e735466bc1c
Robyn Mason <mnorbyn@gmail.com>
2022-02-07 13:45:25 -0600

    new hw

---
+++ Week 2/Pizza Place.js
@@ -0,0 +1,26 @@
+let pizzaPlace = "Anchor Pizza";
+let numberOfToppings = 16;
+
+console.log(pizzaPlace);
+console.log(numberOfToppings);
+console.log(typeof pizzaPlace);
+console.log(typeof numberOfToppings);
+
+console.log(`At ${pizzaPlace} we offer over ${numberOfToppings} delicious toppings.`)
+
+let toppings = 10;
+
+if (toppings < 10 ) {
+    console.log("Quality, not quantity.");
+
+} else if (toppings >= 10) {
+console.log("A whole lot of pizza.");
+
+}
+
+
+
+
+
+
+
